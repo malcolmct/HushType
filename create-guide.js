1ac52e9e0ad8542a4187a24197b801f2997dc495
Malcolm Taylor <malcolm@rampartsystems.com>
2026-02-09 00:18:29 -0500

    Release 1.31

--- create-guide.js
+++ create-guide.js
@@ -23,7 +23,7 @@ const {
 // Configuration
 // ---------------------------------------------------------------------------
 
-const VERSION = "Version 1.30";
+const VERSION = "Version 1.31";
 const CREATION_DATE = "8 February 2026";
 const OUTPUT_FILE = path.join(__dirname, "HushType-User-Guide.docx");
 const SCREENSHOT_DIR = path.join(__dirname, "docs", "screenshots");
